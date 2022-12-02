178ca4d9eb08d9b520cd6610a95ac64ed5e6318e
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-12-02 21:37:38 +0600

    Added jwt access and refresh token.

--- 11TUT/server.js
+++ 11TUT/server.js
@@ -4,11 +4,18 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
 
 const app = express();
 
 // Custom logger middleware
 app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
 
 // Third-party middleware
 app.use(cors(corsOptions)); // Cross Origin Resource Sharing
@@ -16,12 +23,21 @@ app.use(cors(corsOptions)); // Cross Origin Resource Sharing
 // Built-in middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+// serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
