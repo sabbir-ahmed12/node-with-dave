0a602153943f7bc97dd9faacac23e1ab176557fc
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-12-03 19:02:51 +0600

    Completed async CRUD functions.

--- 15TUT/controllers/refreshTokenController.js
+++ 15TUT/controllers/refreshTokenController.js
@@ -1,30 +1,21 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUser: function (user) {
-    this.users = user;
-  },
-};
-
+const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies;
 
-  if (!cookies?.jwt) return res.sendStatus(401);
+  if (!cookies?.jwt) return res.status(401);
   console.log(cookies.jwt);
 
   const refreshToken = cookies.jwt;
 
-  const foundUser = usersDB.users.find(
-    (person) => person.refreshToken === refreshToken
-  );
+  const foundUser = await User.findOne({ refreshToken }).exec();
 
-  if (!foundUser) return res.sendStatus(403);
+  if (!foundUser) return res.status(403);
 
   // evaluate jwt
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || foundUser.username !== decoded.username)
-      return res.sendStatus(403);
+    if (err || foundUser.username !== decoded.username) return res.status(403);
 
     const roles = Object.values(foundUser.roles);
     const accessToken = jwt.sign(
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # node-with-dave
-- This repository contains the files used to learn Node.js watching tutorials of Dave Gray.
+
+- This repository contains all the files needed to build the backend of an application having all the functionalities from building a server with express to async CRUD operations.
+- All the files were created while practicing with Dave Gray.
 - 04TUT does not exist as that lesson taught about Node Package Manager (NPM).
