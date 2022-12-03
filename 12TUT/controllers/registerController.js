4e0b99b2c59dab2a60260123d8709a321c05d625
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-12-03 11:29:49 +0600

    Added user roles for different CRUD operations.

--- 12TUT/controllers/registerController.js
+++ 12TUT/controllers/registerController.js
@@ -27,6 +27,7 @@ const handleNewUser = async (req, res) => {
     // Store the new user
     const newUser = {
       username: user,
+      roles: { User: 2001 },
       password: hashedPwd,
     };
     usersDB.setUser([...usersDB.users, newUser]);
