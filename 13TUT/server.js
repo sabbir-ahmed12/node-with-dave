500aec7c41c7c5d940cd4ecf49d54ca3b2ac111c
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-12-03 15:59:11 +0600

    Connected to mongodb atlas.

--- 13TUT/server.js
+++ 13TUT/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -7,6 +8,11 @@ const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
@@ -55,4 +61,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB.");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
